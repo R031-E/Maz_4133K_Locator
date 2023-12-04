817e32c18ce328174719ed566d5afd6ae0e5c173
Nikita Maz <nikitamaz@hotmail.com>
2023-12-04 18:09:41 +0300

    cmakepreset for linux added

---
+++ out/build/linux-default/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nikit/source/repos/Maz_4133K_Locator/out/build/linux-default/_deps/googletest-src
+# Build directory: C:/Users/nikit/source/repos/Maz_4133K_Locator/out/build/linux-default/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ out/build/linux-default/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nikit/source/repos/Maz_4133K_Locator/out/build/linux-default/_deps/googletest-src/googletest
+# Build directory: C:/Users/nikit/source/repos/Maz_4133K_Locator/out/build/linux-default/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
